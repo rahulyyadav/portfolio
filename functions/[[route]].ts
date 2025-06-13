a0bbf0cbec3221e7c175afb33710a85931a4def1
Rahul Yadav <rahulyyadav21@gmail.com>
2025-06-13 23:19:28 +0545

    version changes

--- functions/[[route]].ts
+++ functions/[[route]].ts
@@ -13,8 +13,16 @@ export async function onRequest(context: Context) {
   const url = new URL(context.request.url);
 
   // If it's the root path, serve index.html
-  if (url.pathname === "/") {
-    return context.env.ASSETS.fetch(new Request(url.origin + "/index.html"));
+  if (url.pathname === "/" || url.pathname === "") {
+    const response = await context.env.ASSETS.fetch(
+      new Request(url.origin + "/index.html")
+    );
+    return new Response(response.body, {
+      headers: {
+        "content-type": "text/html",
+        ...response.headers,
+      },
+    });
   }
 
   // For all other paths, forward to the Next.js application
