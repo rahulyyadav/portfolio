dcaba163ea39bb93985ee4c34fa80141d640646e
Rahul Yadav <rahulyyadav21@gmail.com>
2025-06-14 00:23:33 +0545

    version changes

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@
 const nextConfig = {
   images: {
     domains: ["raba.pages.dev", "github.com"],
+    unoptimized: true,
   },
   distDir: ".next",
   output: "export",
   trailingSlash: true,
+  basePath: "",
 };
 
 module.exports = nextConfig;
