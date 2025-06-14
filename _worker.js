bb41b809bc8926a1116710004c7cdbd2ba3dba3b
Rahul Yadav <rahulyyadav21@gmail.com>
2025-06-14 18:05:54 +0545

    version changes

--- _worker.js
+++ _worker.js
@@ -2,12 +2,12 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
-    // If it's /other, serve the Next.js landing page
-    if (url.pathname === "/other" || url.pathname === "/other/") {
-      return env.ASSETS.fetch(new Request(new URL("/", request.url)));
+    // Serve the static landing page at root
+    if (url.pathname === "/" || url.pathname === "/index.html") {
+      return env.ASSETS.fetch(new Request(new URL("/index.html", request.url)));
     }
 
-    // For all other paths, serve index.html
+    // For all other routes, serve the default React entrypoint (Next.js app shell)
     return env.ASSETS.fetch(new Request(new URL("/index.html", request.url)));
   },
 };
