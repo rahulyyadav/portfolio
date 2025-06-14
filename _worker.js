5c97e880d9367deccca0479d50f7c20dac472f4c
Rahul Yadav <rahulyyadav21@gmail.com>
2025-06-14 17:37:37 +0545

    version changes

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ export default {
 
     // If it's /other, serve the Next.js landing page
     if (url.pathname === "/other" || url.pathname === "/other/") {
-      return env.ASSETS.fetch(new Request(new URL("/other.html", request.url)));
+      return env.ASSETS.fetch(new Request(new URL("/", request.url)));
     }
 
     // For all other paths, serve index.html
