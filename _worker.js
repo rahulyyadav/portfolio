f9ba27c444320bf8bb824a20d531f82b5f94cfe1
Rahul Yadav <rahulyyadav21@gmail.com>
2025-06-14 08:14:54 +0545

    version changes

--- _worker.js
+++ _worker.js
@@ -2,12 +2,7 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
-    // If it's the root path, serve index.html
-    if (url.pathname === "/") {
-      return env.ASSETS.fetch(new Request(`${url.origin}/index.html`));
-    }
-
-    // For everything else, pass through
-    return env.ASSETS.fetch(request);
+    // For all paths, serve index.html
+    return env.ASSETS.fetch(new Request(`${url.origin}/index.html`));
   },
 };
