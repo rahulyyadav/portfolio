95921302e5567b297ba49b7810a0183dc05bc7e0
Rahul Yadav <rahulyyadav21@gmail.com>
2025-06-14 08:56:38 +0545

    version changes

--- _worker.js
+++ _worker.js
@@ -1,5 +1,13 @@
 export default {
   async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+
+    // If it's /other, serve the Next.js landing page
+    if (url.pathname === "/other") {
+      return env.ASSETS.fetch(request);
+    }
+
+    // For all other paths, serve index.html
     return env.ASSETS.fetch(new Request(new URL("/index.html", request.url)));
   },
 };
